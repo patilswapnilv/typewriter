b80b60f135d9b04c14a54c4b1a850bf1f389ed27
Carl-Gustaf Harroch <carl@novoda.com>
2012-05-17 13:22:40 +0100

    adding scaladoc

--- project/build.scala
+++ project/build.scala
@@ -51,7 +51,7 @@ object AndroidBuild extends Build {
   lazy val main = Project(
     "Typewriter",
     file("."),
-    settings = General.settings ++ site.settings ++ ghpages.settings ++ Seq(
+    settings = General.settings ++ site.settings ++ site.includeScaladoc() ++ ghpages.settings ++ Seq(
       git.remoteRepo := "git@github.com:charroch/typewriter.git",
       libraryDependencies ++= Seq(
         "org.scalatest" %% "scalatest" % "1.8.RC1" % "test",
