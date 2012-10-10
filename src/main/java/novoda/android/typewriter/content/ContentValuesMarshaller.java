5ae1e37999a45faf33af36296614076d59f78956
Carl Harroch <carl@novoda.com>
2012-10-10 16:26:01 +0100

    Adding type to content Values

--- src/main/java/novoda/android/typewriter/content/ContentValuesMarshaller.java
+++ src/main/java/novoda/android/typewriter/content/ContentValuesMarshaller.java
@@ -3,10 +3,12 @@ package novoda.android.typewriter.content;
 import android.content.ContentValues;
 import novoda.android.typewriter.Marshaller;
 
-public class ContentValuesMarshaller<T> implements Marshaller<ContentValues, T> {
+public abstract class ContentValuesMarshaller<T> implements Marshaller<ContentValues, T> {
 
     @Override
     public ContentValues marshall(T from, Class<ContentValues> as) {
-        return null;
+        return from(from);
     }
+
+    abstract ContentValues from(T from);
 }
