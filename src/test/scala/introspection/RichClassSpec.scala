7042526bff535f76d653ad43a7730059552af474
frankiesardo <fran.sardo@gmail.com>
2013-05-30 10:44:15 +0100

    Rewrite test to accommodate constructor change

--- src/test/scala/introspection/RichClassSpec.scala
+++ src/test/scala/introspection/RichClassSpec.scala
@@ -8,11 +8,13 @@ import novoda.android.typewriter.cursor.MyObject
 import org.scalatest.matchers._
 
 trait RichClassMatcher {
+
   def method(expectedValue: String) =
-    new HavePropertyMatcher[RichClass, String] {
-      def apply(klass: RichClass) =
+
+    new HavePropertyMatcher[RichClass[_], String] {
+      def apply(klass: RichClass[_]) =
         HavePropertyMatchResult(
-          klass.hasMethod(expectedValue) == true,
+          klass.hasMethod(expectedValue),
           "method",
           expectedValue,
           klass.getClass.getMethods.toString
@@ -20,61 +22,55 @@ trait RichClassMatcher {
     }
 }
 
+class TestObject(
+                  @BeanProperty var nameCamelCase: String,
+                  @BeanProperty var name: String,
+                  @BeanProperty var id: Int,
+                  @BeanProperty var short: Short,
+                  @BeanProperty var long: Long,
+                  @BeanProperty var float: Float,
+                  @BeanProperty @Mapper("some_value") var mappedString: String
+                  ) {
+  @Mapper("some_value")
+  def mapped(s: String) {}
+}
+
 class RichClassSpec extends WordSpec with ShouldMatchers with RichClassMatcher {
   "a rich class" should {
 
     "find correct setter" in {
-      RichClass().setter("name") should be(getMethod("setName", classOf[String]))
-      RichClass().setter("name_camel_case") should be(getMethod("setNameCamelCase", classOf[String]))
+      new RichClass(classOf[TestObject]).setter("name") should be(getMethod("setName", classOf[String]))
+      new RichClass(classOf[TestObject]).setter("name_camel_case") should be(getMethod("setNameCamelCase", classOf[String]))
     }
 
     "throw an exception if getMethod not found" in {
       evaluating(
-        RichClass().setter("name_not_found")
+        new RichClass(classOf[TestObject]).setter("name_not_found")
       ) should produce[NoSuchMethodException]
     }
 
     "find correct type setter" in {
-      RichClass().setter("name") should be(getMethod("setName", classOf[String]))
-      RichClass().setter("id") should be(getMethod("setId", classOf[Int]))
-      RichClass().setter("short") should be(getMethod("setShort", classOf[Short]))
-      RichClass().setter("long") should be(getMethod("setLong", classOf[Long]))
-      RichClass().setter("float") should be(getMethod("setFloat", classOf[Float]))
+      new RichClass(classOf[TestObject]).setter("name") should be(getMethod("setName", classOf[String]))
+      new RichClass(classOf[TestObject]).setter("id") should be(getMethod("setId", classOf[Int]))
+      new RichClass(classOf[TestObject]).setter("short") should be(getMethod("setShort", classOf[Short]))
+      new RichClass(classOf[TestObject]).setter("long") should be(getMethod("setLong", classOf[Long]))
+      new RichClass(classOf[TestObject]).setter("float") should be(getMethod("setFloat", classOf[Float]))
     }
 
     "map the correct setter using annotation" in {
-      RichClass().setter("some_value") should be(getMethod("mapped", classOf[String]))
+      new RichClass(classOf[TestObject]).setter("some_value") should be(getMethod("mapped", classOf[String]))
       new RichClass(classOf[MyObject]).setter("some_value") should be(classOf[MyObject].getMethod("setTest2", classOf[String]))
     }
 
     "have the getMethod" in {
       List("name_camel_case", "name", "id", "short", "long", "float", "some_value").foreach(
-        (methodName) => RichClass() should have(method(methodName))
+        (name) => new RichClass(classOf[TestObject]) should have (method(name))
       )
     }
 
     def getMethod(mn: String, c: Class[_]) = {
-      classOf[RichClass.TestObject].getMethod(mn, c)
+      classOf[TestObject].getMethod(mn, c)
     }
-
-    object RichClass {
-
-      case class TestObject(
-                             @BeanProperty var nameCamelCase: String,
-                             @BeanProperty var name: String,
-                             @BeanProperty var id: Int,
-                             @BeanProperty var short: Short,
-                             @BeanProperty var long: Long,
-                             @BeanProperty var float: Float,
-                             @BeanProperty @Mapper("some_value") var mappedString: String
-                             ) {
-        @Mapper("some_value")
-        def mapped(s: String) {}
-      }
-
-      def apply() = new RichClass(classOf[TestObject])
-    }
-
 
   }
 }
